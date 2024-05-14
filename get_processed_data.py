6a43e1fc9e521d6e2d73cd28cb697eb74349a1f7
Malte <hi@malts.me>
2024-05-14 12:53:24 +0200

    upload new measurements

---
+++ get_processed_data.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+
+def process_file(file_path, start_ts, end_ts):
+    raw_data = np.genfromtxt(
+        file_path,
+        delimiter=",",
+        skip_header=12,
+        names=[
+            "PacketCounter",
+            "SampleTimeFine",
+            "Quat_W",
+            "Quat_X",
+            "Quat_Y",
+            "Quat_Z",
+            "FreeAcc_X",
+            "FreeAcc_Y",
+            "FreeAcc_Z",
+        ],
+        dtype=(int, int, float, float, float, float, float, float, float),
+    )
+
+    # normalize start to 0
+    raw_data["SampleTimeFine"] -= raw_data["SampleTimeFine"][0]
+
+    # drop all rows where SampleTimeFine is smaller than start_ts and larger than end_ts
+    data = raw_data[
+        (raw_data["SampleTimeFine"] >= start_ts)
+        & (raw_data["SampleTimeFine"] <= end_ts)
+    ]
+
+    # normalize again
+    data["SampleTimeFine"] -= data["SampleTimeFine"][0]
+
+    return data
+
+
+processed_data_malte = {
+    "3": [process_file("measurements/2024-04-30/malte-3.csv", 13848061, 41408943)],
+    # "4": [process_file("measurements/2024-04-30/malte-4.csv", 12878159, 50263354)],
+}
+
+
